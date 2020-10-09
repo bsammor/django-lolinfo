5ecbf5475905eac182c0c3c83639ca0149fac3b0
Ubuntu <ubuntu@ip-172-31-19-5.eu-west-2.compute.internal>
2020-10-09 01:33:43 +0000

    fixed statc_url

--- lolinfo/lolinfo/settings.py
+++ lolinfo/lolinfo/settings.py
@@ -122,6 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/django-lolinfo/'
 
 STATIC_ROOT = '/var/www/html/django-lolinfo/'
